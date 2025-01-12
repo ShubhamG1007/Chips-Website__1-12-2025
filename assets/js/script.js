2e99026ec1c3eaf9ffaccd188d966ed9ecbd5d54
NN-Bayat <noorullahbayat1414@gmail.com>
2025-01-12 18:09:54 +0430

    assets file add

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
+
+// Show menu
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+};
+
+// Menu hidden
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+};
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu');
+    // When we click on each nav__link, we remove the show-menu....
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*=============== SHADOW HEADER ===============*/
+const scrollHeader = () => {
+    const header = document.getElementById('header');
+    // Add or remove class based on scroll position
+    window.scrollY >= 40 ? header.classList.add('shadow-header') : header.classList.remove('shadow-header');
+};
+window.addEventListener('scroll', scrollHeader);
+
+/*=============== SWIPER FAVORITES ===============*/
+const swiperFavorites = new Swiper('.favorites__swiper', {
+    loop: true,
+    grabCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto'
+})
+
+/*=============== SHOW SCROLL UP ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
